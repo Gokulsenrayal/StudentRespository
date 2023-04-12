46510be38d36abf99bff8168d5a2c67986d1cab4
Gokula Krishna S <gokula.krishna@star-knowledge.org>
2023-04-12 09:57:50 +0530

    UI Modification

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace LinqStoringData
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Subjects", action = "index", id = UrlParameter.Optional }
+                defaults: new { controller = "Grid", action = "GridFactor", id = UrlParameter.Optional }
             );
         }
     }
